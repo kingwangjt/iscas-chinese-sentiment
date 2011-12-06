02deaf51bc87b4dc5cba3e495995cd027dcf5a52
xukexin1988hpxcm <xukexin1988hpxcm@163.com>
2011-12-06 17:40:24 +0000

    fd

--- src/cn/ac/iscas/entrance/HelloWorld.java
+++ src/cn/ac/iscas/entrance/HelloWorld.java
@@ -6,6 +6,6 @@ package cn.ac.iscas.entrance;
  */
 public class HelloWorld {
 	public static void main(String[] args){
-		System.out.println("Hello world, First submission is mine, hiahia~");
+		System.out.println("Hello wforld, First submission is mine, hiahia~");
 	}
 }
