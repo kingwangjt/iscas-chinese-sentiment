abbf7a4a07833bd8943b822f4451431ad9197293
wuhanyu@gmail.com <wuhanyu@gmail.com@b6345733-8bfd-4813-506d-52e8e1d75cc8>
2011-12-09 02:33:07 +0000

    create standard IO class for convenient.

---
+++ trunk/src/cn/as/iscas/io/SmartFile.java
@@ -0,0 +1,22 @@
+package cn.as.iscas.io;
+
+import java.io.File;
+
+public class SmartFile {
+	File file;
+	public void initFile(String path, boolean isRead) {
+	}
+	
+	public void closeFile() {
+		
+	}
+	
+	public boolean readLine(){
+		boolean flag = true;
+		return flag;
+	}
+	
+	public void writeLine(String line){
+
+	}
+}
